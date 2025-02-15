b4ae0c53effa53186d45f12b8555ace4b0493b57
AlbaCastilla <castillamackin@gmail.com>
2025-02-15 17:36:26 +0100

    Footer v1 metido, no consigo que se ajuste al ancho de la pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 
-// src/App.js
+//
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar.jsx';
 import Home from './pages/Home';
 import Viajes from './pages/Viajes.jsx';
 import Comida from './pages/Comida';
 import DayInMyLife from './pages/DayInMyLife';
 import Otros from './pages/Otros.jsx';
+import Footer from './components/footer/Footer.jsx';
+
+
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar /> {/* Aquí se usa el Navbar */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/viajes" element={<Viajes />} />
-        <Route path="/comida" element={<Comida />} />
-        <Route path="/day-in-my-life" element={<DayInMyLife />} />
-        <Route path="/otros" element={<Otros />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/viajes" element={<Viajes />} />
+          <Route path="/comida" element={<Comida />} />
+          <Route path="/day-in-my-life" element={<DayInMyLife />} />
+          <Route path="/otros" element={<Otros />} />
+        </Routes>
+      </Router>
+      <Footer /> 
+      {/* he sacado el footer del router pq no se 
+      como hacer que se ajuste al ancho de la pantalla 
+      pero antes estaba dentrp */}
+    </div>
   );
 }
 
