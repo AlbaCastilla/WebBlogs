f1848760602600928e70f20ce85b90eb50166a83
AlbaCastilla <castillamackin@gmail.com>
2025-02-15 16:38:55 +0100

    Navbar v1 y componentes de cada pagina creados
    Componente de navbar con funcionalidad creado para adaptar a nuestro diseño final. Además páginas creadas para adaptar.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+
+const Navbar = () => {
+  {/*Estado para controlar si el menú está abierto o cerrado*/}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  {/* Función para alternar el estado del menú*/}
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <h1 className="logo">Mi Blog</h1>
+      {/* menu-icon = Icono de menú hamburguesa n pantallas pequeñas */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* Menu de nav */}
+      <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+        <ul>
+          {/*<Link> <Link/> actua como un <a><a/>*/}
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/viajes">Viajes</Link></li>
+          <li><Link to="/comida">Comida</Link></li>
+          <li><Link to="/day-in-my-life">Day in my life</Link></li>
+          <li><Link to="/otros">Otros</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Otros.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Otros = () => {
+  return (
+    <div>
+      <h2>Bienvenido a la Página de otros posts</h2>
+      <p>Esta es la sección de posts de cualquier otra cosa que no sea comida, day in my life o viajes</p>
+    </div>
+  );
+};
+
+export default Otros;
