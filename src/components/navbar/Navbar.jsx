7fbc2390ec0e39747417e0e5ff4b690e846f0ac4
beamm536 <beamm023@gmail.com>
2025-03-01 22:46:20 +0100

    navbar cambiados los estilos

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,10 @@
 // import './Navbar.css'; 
 
 // const Navbar = () => {
-//   {/*Estado para controlar si el menú está abierto o cerrado*/}
+//   {/* Estado para controlar si el menú está abierto o cerrado */}
 //   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-//   {/* Función para alternar el estado del menú*/}
+//   {/* Función para alternar el estado del menú */}
 //   const toggleMenu = () => {
 //     setIsMenuOpen(!isMenuOpen);
 //   };
@@ -14,23 +14,25 @@
 //   return (
 //     <nav className="navbar">
 //       <h1 className="logo">Blog de blogs</h1>
-//       {/* menu-icon = Icono de menú hamburguesa n pantallas pequeñas */}
+//       {/* menu-icon = Icono de menú hamburguesa en pantallas pequeñas */}
 //       <div className="menu-icon" onClick={toggleMenu}>
 //         <div className="bar"></div>
 //         <div className="bar"></div>
 //         <div className="bar"></div>
 //       </div>
 
-//       {/* Menu de nav */}
+//       {/* Menú de navegación */}
 //       <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
 //         <ul>
-//           {/*<Link> <Link/> actua como un <a><a/>*/}
+//           {/* <Link> actúa como un <a> */}
 //           <li><Link to="/">Home</Link></li>
 //           <li><Link to="/crear-blogs">Blogs</Link></li>
 //           <li><Link to="/viajes">Viajes</Link></li>
 //           <li><Link to="/comida">Comida</Link></li>
 //           <li><Link to="/day-in-my-life">Day in my life</Link></li>
 //           <li><Link to="/otros">Otros</Link></li>
+//           <li><Link to="/registro">Registro</Link></li> 
+//           <li><Link to="/login">Login</Link></li> 
 //         </ul>
 //       </div>
 //     </nav>
@@ -39,45 +41,57 @@
 
 // export default Navbar;
 
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; 
 
-const Navbar = () => {
-  {/* Estado para controlar si el menú está abierto o cerrado */}
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+ import { useState } from 'react';
+ import { Link } from 'react-router-dom';
+ import './Navbar.css';
 
-  {/* Función para alternar el estado del menú */}
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+ const Navbar = () => {
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  return (
-    <nav className="navbar">
-      <h1 className="logo">Blog de blogs</h1>
-      {/* menu-icon = Icono de menú hamburguesa en pantallas pequeñas */}
-      <div className="menu-icon" onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
+   const toggleMenu = () => {
+     setIsMenuOpen(!isMenuOpen);
+   };
 
-      {/* Menú de navegación */}
-      <div className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
-        <ul>
-          {/* <Link> actúa como un <a> */}
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/crear-blogs">Blogs</Link></li>
-          <li><Link to="/viajes">Viajes</Link></li>
-          <li><Link to="/comida">Comida</Link></li>
-          <li><Link to="/day-in-my-life">Day in my life</Link></li>
-          <li><Link to="/otros">Otros</Link></li>
-          <li><Link to="/registro">Registro</Link></li> 
-          <li><Link to="/login">Login</Link></li> 
-        </ul>
-      </div>
-    </nav>
-  );
-};
+   return (
+     <nav>
+       <div className="logo">
+         <img src="/assets/Logo64x64.png" alt="logo" />
+         <h1>Blog de blogs</h1>
+       </div>
+       <ul className={isMenuOpen ? 'active' : ''}>
+         <li><Link to="/">Home</Link></li>
+         <li><Link to="/crear-blogs">Blogs</Link></li>
+         <li><Link to="/viajes">Viajes</Link></li>
+         <li><Link to="/comida">Comida</Link></li>
+         <li><Link to="/day-in-my-life">Day in my life</Link></li>
+         <li><Link to="/otros">Otros</Link></li>
+         <li><Link to="/registro">Registro</Link></li>
+         <li><Link to="/login">Login</Link></li>
+       </ul>
+       <div className={`hamburger ${isMenuOpen ? 'hamburger-active' : ''}`} onClick={toggleMenu}>
+         <span className="line"></span>
+         <span className="line"></span>
+         <span className="line"></span>
+       </div>
+       <div className={`menubar ${isMenuOpen ? 'active' : ''}`}>
+         <ul>
+           <li><Link to="/">Home</Link></li>
+           <li><Link to="/crear-blogs">Blogs</Link></li>
+           <li><Link to="/viajes">Viajes</Link></li>
+           <li><Link to="/comida">Comida</Link></li>
+           <li><Link to="/day-in-my-life">Day in my life</Link></li>
+           <li><Link to="/otros">Otros</Link></li>
+           <li><Link to="/registro">Registro</Link></li>
+           <li><Link to="/login">Login</Link></li>
+         </ul>
+       </div>
+     </nav>
+   );
+ };
 
-export default Navbar;
+ export default Navbar;
+
+
+
+
