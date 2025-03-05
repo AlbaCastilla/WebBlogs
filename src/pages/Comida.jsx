8a36dd666604ac5a5a6d86603c9b539636627121
Pedromrtz <pmartinezigual@gmail.com>
2025-03-05 10:23:46 +0100

    Post Comida Listar

--- src/pages/Comida.jsx
+++ src/pages/Comida.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react';
+import PostComida from '../components/post/post-list/comida/PostComida';
 
 const Comida = () => {
   return (
     <div>
       <h2>Bienvenido a la Página de Comida</h2>
       <p>Esta es la sección de posts de blogs de comida</p>
+      <PostComida/>
     </div>
   );
 };
